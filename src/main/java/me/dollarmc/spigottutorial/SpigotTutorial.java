4e4b5a09eadcdbd19813aa16eed2c7092f548430
arai1999 <arai1999@hotmail.com>
2024-03-26 18:24:56 -0700

    Inital plugin development, simply outputting to console

--- src/main/java/me/dollarmc/spigottutorial/SpigotTutorial.java
+++ src/main/java/me/dollarmc/spigottutorial/SpigotTutorial.java
@@ -7,11 +7,12 @@ public final class SpigotTutorial extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Plugin Started");
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Plugin Stopped");
     }
 }
