86821c7acbead59b16da64fe90bd80a4b372deab
arai1999 <arai1999@hotmail.com>
2024-03-26 19:46:13 -0700

    Basic event handlers in main

--- src/main/java/me/dollarmc/spigottutorial/SpigotTutorial.java
+++ src/main/java/me/dollarmc/spigottutorial/SpigotTutorial.java
@@ -1,13 +1,34 @@
 package me.dollarmc.spigottutorial;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SpigotTutorial extends JavaPlugin {
+public final class SpigotTutorial extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
         System.out.println("Plugin Started");
+        // Register events
+        getServer().getPluginManager().registerEvents(this, this);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        // Event handler when a player joined
+        String playerName = event.getPlayer().getName();
+        event.setJoinMessage("Bitchass " + playerName + " joined the server");
+    }
+
+    @EventHandler
+    public void onLeaveBed(PlayerBedLeaveEvent event) {
+        Player player = event.getPlayer();
+        player.setHealth(0);
+        player.sendMessage("You left a bed, die fool");
     }
 
     @Override
